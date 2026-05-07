6f74d2a3d0898d8a977c4052231de81dcefc8e93
KentRecede <kentrecede1@gmail.com>
2026-05-07 13:06:25 +0800

    fixed firebase.ts

---
+++ app/lib/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBl5KnceoQOftbo7iYB3DhHbvKjctJibis",
+  authDomain: "food-delivery-app-acff2.firebaseapp.com",
+  projectId: "food-delivery-app-acff2",
+  storageBucket: "food-delivery-app-acff2.firebasestorage.app",
+  messagingSenderId: "877648006121",
+  appId: "1:877648006121:web:4a97cf3d85c66ee520d978",
+  measurementId: "G-PQ4FGXX5KY"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
